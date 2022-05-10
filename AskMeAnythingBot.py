e1c831fd22f9e5e94713f78892f271e3c16ef314
One5hot76 <ftyre76@gmail.com>
2022-05-10 09:15:52 -0500

    Get things started
    We have to start somewhere.

---
+++ AskMeAnythingBot.py
@@ -0,0 +1,8 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = '.' / '.env'
+load_dotenv(dotenv_path = env_path)
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
